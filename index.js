bfc7d422463d58b3930bab5dcb0dd7a9da751252
Josh Bright <josh.bright@gmail.com>
2023-03-21 21:10:31 -0700

    Added function to draw initial cells

--- index.js
+++ index.js
@@ -20,7 +20,22 @@ const drawGrid = (cellSize, cellsWide, cellsHigh, canvas) => {
         });
         canvas.add(newLine);
     }    
-}
+};
+
+const drawCells = (cellSize, cells, canvas) => {
+    cells.forEach((cell) => {
+        const x = cell[0] * cellSize;
+        const y = cell[1] * cellSize;
+
+        canvas.add(new fabric.Rect({
+            left: x,
+            top: y,
+            width: cellSize,
+            height: cellSize,
+            fill: "red"
+        }));
+    })
+};
 
 canvas.setWidth(cellSize * cellsWide + 1);
 canvas.setHeight(cellSize * cellsHigh + 1);
